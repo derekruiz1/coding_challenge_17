fec043c80bf1b3a9d6fb26684310289e210058b1
Derek Ruize <derekruiz@usf.edu>
2025-03-30 21:55:11 -0400

    Task 4 - Finalizing Reporting System

--- cc_17.js
+++ cc_17.js
@@ -9,7 +9,7 @@ class Customer { //Making Customer Class
         this.purchaseHistory.push(amount)
     }
     getTotalSpent(){ //Shows purchase history
-        console.log(this.purchaseHistory.reduce(sum , purchase => sum + purchase ,0))
+        console.log(this.purchaseHistory.reduce((sum , purchase) => sum + purchase ,0))
     }
 }
 
@@ -32,7 +32,7 @@ class SalesRep { //Making Sales Rep Class
 }
 
 //Task 3 
-class VIPCustomer extends customer { //VIP Class
+class VIPCustomer extends Customer { //VIP Class
     constructor(name, email, vipLevel) {
         super(name, email)
         this.vipLevel = vipLevel
@@ -43,3 +43,40 @@ class VIPCustomer extends customer { //VIP Class
         console.log(`VIP${this.name},Total:${this.bonusTotal}`)
     }
 }
+
+//Task 4
+//Customer Data
+
+const customer1 = new Customer("John Doe", "john@example.com");
+customer1.addPurchase(100);
+customer1.addPurchase(50);
+
+const customer2 = new Customer("Susan Doe", "susan@example.com");
+customer2.addPurchase(75);
+
+const vipCustomer1 = new VIPCustomer("Jane Smith", "jane@example.com", "Gold");
+vipCustomer1.addPurchase(200);
+vipCustomer1.addPurchase(100);
+
+const vipCustomer2 = new VIPCustomer("Steve Carell", "steve@theoffice.com", "Gold");
+vipCustomer2.addPurchase(500);
+
+const salesRep = new SalesRep("Dwight");
+salesRep.addClient(customer1);
+salesRep.addClient(customer2);
+salesRep.addClient(vipCustomer1);
+salesRep.addClient(vipCustomer2);
+
+const totalRevenue = this.clients.reduce((sum, client) => {
+    return sum + client.purchaseHistory.reduce((clientSum, purchase) => clientSum + purchase, 0);
+}, 0);
+console.log(`Total Revenue:${totalRevenue}`)
+
+const highSpendingCustomers = this.clients.filter(client => client.getTotalSpent() >= 500);
+console.log("High-spending customers:", highSpenders.map(client => client.name));
+
+const customerSummary = this.clients.map(client => ({
+    name: client.name,
+    totalSpent: client.getTotalSpent(),
+}));
+console.log("Customer Summary:", customerSummary);
